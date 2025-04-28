b77510934d3d926cfbf85285fba1837b7f566cd7
dxcezza <48048129+dxcezza@users.noreply.github.com>
2025-04-28 10:16:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,9 @@ app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 CORS(app)
-login_manager = LoginManager(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'auth_page'
 
 # Модель пользователя
 class User(db.Model, UserMixin):
